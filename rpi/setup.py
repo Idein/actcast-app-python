cfcd1f6ba12857c046fb8224665928048972ccb9
Takeo Imai <takeo.bono@gmail.com>
2019-04-18 18:56:37 +0900

    fix around chroot things

--- rpi/setup.py
+++ rpi/setup.py
@@ -5,12 +5,7 @@ import setuptools
 setuptools.setup(
     name="actcast",
     version="0.0.1",
-    author="Takeo Imai",
-    author_email="bonotake.oshigoto@gmail.com",
     description="Python API for Actcast apps",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Idein/actcast-py",
     packages=setuptools.find_packages(),
     install_requires=['picamera'],
     python_requires='~=3.5.3',
