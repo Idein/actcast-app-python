73c4921f7f53a74d1fd8de9b35e7e76e5d8177e0
notogawa <n.ohkawa@gmail.com>
2019-04-22 23:36:11 +0900

    default heartbeat

--- actfw/__init__.py
+++ actfw/__init__.py
@@ -1,4 +1,5 @@
-import json
+import os, json
+from pathlib import Path
 import actfw.task
 import actfw.capture
 from .application import Application
@@ -12,3 +13,16 @@ __version__ = _version.__version__
 def notice(notices, *args, **kwargs):
     kwargs['flush'] = True
     print(json.dumps(notices), *args, **kwargs)
+
+_default_heartbeat_file = Path('/root/heartbeat')
+
+def _default_heartbeat():
+    _default_heartbeat_file.touch()
+
+_heartbeat_function = _default_heartbeat
+
+def set_heartbeat_function(f):
+    _heartbeat_function = f
+
+def heartbeat():
+    _heartbeat_function()
