ef3e5ab814cac203939abd73e39628a25b2d8009
notogawa <n.ohkawa@gmail.com>
2019-04-23 01:08:35 +0900

    fix coordinate transformation

--- actfw/display.py
+++ actfw/display.py
@@ -4,23 +4,23 @@ class Display:
 
     def __init__(self, camera, size):
         self.size = size
-        self.preffered_size = EDID().prefferd_mode()
-        scale_w = self.preffered_size[0] / self.size[0]
-        scale_h = self.preffered_size[1] / self.size[1]
+        self.preferred_size = EDID().prefferd_mode()
+        scale_w = self.preferred_size[0] / self.size[0]
+        scale_h = self.preferred_size[1] / self.size[1]
         self.scale = min(scale_w, scale_h)
         self.ofs_w = 0
         self.ofs_h = 0
         if (scale_w > scale_h):
-            self.ofs_w = (self.preffered_size[0] - self.scale * self.size[0]) / 2.0
+            self.ofs_w = (self.preferred_size[0] - self.scale * self.size[0]) / 2.0
         else:
-            self.ofs_h = (self.preffered_size[1] - self.scale * self.size[1]) / 2.0
+            self.ofs_h = (self.preferred_size[1] - self.scale * self.size[1]) / 2.0
         self.camera = camera
         self.layer = None
 
     def update(self, dst_rect, src_buf, src_size, src_format):
         rect = (
-            int(dst_rect[0] * self.scale + self.ofs_w),
-            int(dst_rect[1] * self.scale + self.ofs_h),
+            int((dst_rect[0] - self.size[0] / 2.0) * self.scale + (self.preferred_size[0] - self.ofs_w) / 2.0),
+            int((dst_rect[1] - self.size[1] / 2.0) * self.scale + (self.preferred_size[1] - self.ofs_h) / 2.0),
             int(dst_rect[2] * self.scale),
             int(dst_rect[3] * self.scale),
         )
