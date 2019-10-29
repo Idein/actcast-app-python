7e1ab5f79967e1ea019df3e28d07dce424b403ee
notogawa <n.ohkawa@gmail.com>
2019-10-29 09:50:53 +0900

    bounds queue_id

--- actfw/task/pipe.py
+++ actfw/task/pipe.py
@@ -30,9 +30,9 @@ class Pipe(Task):
         length = len(self.in_queues)
         while self._is_running():
             try:
-                i = self.in_queues[in_queue_id % length].get(timeout=1)
+                i = self.in_queues[in_queue_id].get(timeout=1)
                 yield i
-                in_queue_id += 1
+                in_queue_id = (in_queue_id + 1) % length
             except Empty:
                 pass
             except GeneratorExit:
@@ -44,8 +44,8 @@ class Pipe(Task):
         length = len(self.out_queues)
         while self._is_running():
             try:
-                self.out_queues[self.out_queue_id % length].put(o, timeout=1)
-                self.out_queue_id += 1
+                self.out_queues[self.out_queue_id].put(o, timeout=1)
+                self.out_queue_id = (self.out_queue_id + 1) % length
                 return True
             except Full:
                 pass
