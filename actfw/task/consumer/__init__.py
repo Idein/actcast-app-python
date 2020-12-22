d2921724857b9e5145ce99ec83023dd2a25633b2
Sho Nakatani <lay.sakura@gmail.com>
2020-12-22 09:34:33 +0900

    emits DeprecationWarning when importing actfw

--- actfw/task/consumer/__init__.py
+++ actfw/task/consumer/__init__.py
@@ -1,1 +1,3 @@
 from actfw_core.task.consumer import *
+from warnings import warn
+warn(DeprecationWarning("actfw is DEPRECATED. Use actfw-core instead"))
--- actfw/task/pipe/__init__.py
+++ actfw/task/pipe/__init__.py
@@ -1,1 +1,3 @@
 from actfw_core.task.pipe import *
+from warnings import warn
+warn(DeprecationWarning("actfw is DEPRECATED. Use actfw-core instead"))
